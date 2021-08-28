41c5f69f8e01fb21c178e9d37bd1ba6cb0c74b6e
spencercreer <spencercreer@gmail.com>
2021-08-28 15:32:33 -0700

    setup Sequelize and Gigs models

--- app.js
+++ app.js
@@ -2,8 +2,19 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const path = require('path')
+require('dotenv').config()
+
+const db = require('./config/database')
+
+//Test db connection
+db.authenticate()
+    .then(() => console.log('Database connected...'))
+    .catch(err => console.log('db.authenticate error: ' + err))
 
 const app = express()
+
+// Gig routes
+app.use('/gigs', require('./routes/gigs'))
 
 const PORT = process.env.PORT || 5000
 
---
+++ routes/gigs.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const router = express.Router()
+// require database and models
+const db = require('../config/database')
+const Gig = require('../models/Gig')
+
+
+router.get('/', (req, res) => {
+    Gig.findAll()
+        .then(gigs => {
+            console.log(gigs)
+            res.sendStatus(200)
+        })
+        .catch(err => console.log('/gigs/ err' + err))
+})
+
+module.exports = router
